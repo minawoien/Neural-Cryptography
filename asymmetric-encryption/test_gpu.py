1d0747085386ff0ddc5980277dd41ec703d88174
minawoien <mc.woien@stud.uis.no>
2023-10-22 18:34:50 +0200

    change testing gpu

--- asymmetric-encryption/test_gpu.py
+++ asymmetric-encryption/test_gpu.py
@@ -1,17 +1,11 @@
 import tensorflow as tf
-# Enable log device placement
-physical_devices = tf.config.list_physical_devices('GPU')
-if physical_devices:
-    tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
-# Configure TensorFlow session
-tf.config.set_soft_device_placement(True)
+gpus = tf.config.list_physical_devices('GPU')
 
-# Check the device being used
-with tf.device('GPU:0'):  # Or 'GPU:1' etc. depending on which GPU you want to use
-    a = tf.constant([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
-    b = tf.constant([[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]])
-    c = tf.matmul(a, b)
+# Enable memory growth for all GPUs
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
 
-# Should print something like '/job:localhost/replica:0/task:0/device:GPU:0'
-print(c.device)
+# You can verify the setting with the following
+for gpu in gpus:
+    print(f'{gpu}: Memory growth: {tf.config.experimental.get_memory_growth(gpu)}')
