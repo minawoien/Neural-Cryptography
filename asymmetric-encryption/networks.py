9f6c4be55cc0c02d42487106fd7e0944f292ba84
minawoien <mc.woien@stud.uis.no>
2023-10-22 18:41:09 +0200

    adding code for gpu

--- asymmetric-encryption/networks.py
+++ asymmetric-encryption/networks.py
@@ -14,104 +14,103 @@ pad = 'same'
 # m_train = 1024000
 m_train = 500000
 
+strategy = tf.distribute.MirroredStrategy()
+print('Number of devices: {}'.format(strategy.num_replicas_in_sync))
+with strategy.scope():
+    # Alice network
+    ainput0 = Input(shape=(m_bits))  # message
+    ainput1 = Input(shape=(puk_bits,))  # public key
 
-# Alice network
-ainput0 = Input(shape=(m_bits))  # message
-ainput1 = Input(shape=(puk_bits,))  # public key
+    ainput = concatenate([ainput0, ainput1], axis=1)
 
-ainput = concatenate([ainput0, ainput1], axis=1)
+    adense1 = Dense(units=(m_bits + puk_bits), activation='tanh')(ainput)
+    areshape = Reshape((m_bits + puk_bits, 1,))(adense1)
 
-adense1 = Dense(units=(m_bits + puk_bits), activation='tanh')(ainput)
-areshape = Reshape((m_bits + puk_bits, 1,))(adense1)
+    aconv1 = Conv1D(filters=2, kernel_size=4, strides=1,
+                    padding=pad, activation='tanh')(areshape)
 
-aconv1 = Conv1D(filters=2, kernel_size=4, strides=1,
-                padding=pad, activation='tanh')(areshape)
+    aconv2 = Conv1D(filters=4, kernel_size=2, strides=2,
+                    padding=pad, activation='tanh')(aconv1)
 
-aconv2 = Conv1D(filters=4, kernel_size=2, strides=2,
-                padding=pad, activation='tanh')(aconv1)
+    aconv3 = Conv1D(filters=4, kernel_size=1, strides=1,
+                    padding=pad, activation='tanh')(aconv2)
 
-aconv3 = Conv1D(filters=4, kernel_size=1, strides=1,
-                padding=pad, activation='tanh')(aconv2)
+    aconv4 = Conv1D(filters=1, kernel_size=1, strides=1,
+                    padding=pad, activation='sigmoid')(aconv3)
 
-aconv4 = Conv1D(filters=1, kernel_size=1, strides=1,
-                padding=pad, activation='sigmoid')(aconv3)
+    aoutput = Flatten()(aconv4)
 
-aoutput = Flatten()(aconv4)
+    alice = Model(inputs=[ainput0, ainput1],
+                  outputs=aoutput, name='alice')
 
-alice = Model(inputs=[ainput0, ainput1],
-              outputs=aoutput, name='alice')
+    # Bob network
+    binput0 = Input(shape=(c_bits,))  # ciphertext
+    binput1 = Input(shape=(prk_bits,))  # private key
 
+    binput = concatenate([binput0, binput1], axis=1)
 
-# Bob network
-binput0 = Input(shape=(c_bits,))  # ciphertext
-binput1 = Input(shape=(prk_bits,))  # private key
+    bdense1 = Dense(units=(m_bits*2), activation='tanh')(binput)
+    breshape = Reshape((m_bits*2, 1,))(bdense1)
 
-binput = concatenate([binput0, binput1], axis=1)
+    bconv1 = Conv1D(filters=2, kernel_size=4, strides=1,
+                    padding=pad, activation='tanh')(breshape)
+    bconv2 = Conv1D(filters=4, kernel_size=2, strides=2,
+                    padding=pad, activation='tanh')(bconv1)
+    bconv3 = Conv1D(filters=4, kernel_size=1, strides=1,
+                    padding=pad, activation='tanh')(bconv2)
+    bconv4 = Conv1D(filters=1, kernel_size=1, strides=1,
+                    padding=pad, activation='sigmoid')(bconv3)
 
-bdense1 = Dense(units=(m_bits*2), activation='tanh')(binput)
-breshape = Reshape((m_bits*2, 1,))(bdense1)
+    boutput = Flatten()(bconv4)
 
-bconv1 = Conv1D(filters=2, kernel_size=4, strides=1,
-                padding=pad, activation='tanh')(breshape)
-bconv2 = Conv1D(filters=4, kernel_size=2, strides=2,
-                padding=pad, activation='tanh')(bconv1)
-bconv3 = Conv1D(filters=4, kernel_size=1, strides=1,
-                padding=pad, activation='tanh')(bconv2)
-bconv4 = Conv1D(filters=1, kernel_size=1, strides=1,
-                padding=pad, activation='sigmoid')(bconv3)
+    bob = Model(inputs=[binput0, binput1],
+                outputs=boutput, name='bob')
 
-boutput = Flatten()(bconv4)
+    # # Eve network
+    einput = Input(shape=(c_bits,))  # ciphertext only
 
+    edense1 = Dense(units=(m_bits*2), activation='tanh')(einput)
+    edense2 = Dense(units=(m_bits*2), activation='tanh')(edense1)
+    ereshape = Reshape((m_bits*2, 1,))(edense2)
 
-bob = Model(inputs=[binput0, binput1],
-            outputs=boutput, name='bob')
+    econv1 = Conv1D(filters=2, kernel_size=4, strides=1,
+                    padding=pad, activation='tanh')(ereshape)
+    econv2 = Conv1D(filters=4, kernel_size=2, strides=2,
+                    padding=pad, activation='tanh')(econv1)
+    econv3 = Conv1D(filters=4, kernel_size=1, strides=1,
+                    padding=pad, activation='tanh')(econv2)
+    econv4 = Conv1D(filters=1, kernel_size=1, strides=1,
+                    padding=pad, activation='sigmoid')(econv3)
 
-# # Eve network
-einput = Input(shape=(c_bits,))  # ciphertext only
+    eoutput = Flatten()(econv4)  # Eve's attempt at guessing the plaintext
 
-edense1 = Dense(units=(m_bits*2), activation='tanh')(einput)
-edense2 = Dense(units=(m_bits*2), activation='tanh')(edense1)
-ereshape = Reshape((m_bits*2, 1,))(edense2)
+    eve = Model(einput, eoutput, name='eve')
 
-econv1 = Conv1D(filters=2, kernel_size=4, strides=1,
-                padding=pad, activation='tanh')(ereshape)
-econv2 = Conv1D(filters=4, kernel_size=2, strides=2,
-                padding=pad, activation='tanh')(econv1)
-econv3 = Conv1D(filters=4, kernel_size=1, strides=1,
-                padding=pad, activation='tanh')(econv2)
-econv4 = Conv1D(filters=1, kernel_size=1, strides=1,
-                padding=pad, activation='sigmoid')(econv3)
+    # Loss and optimizer
+    aliceout = alice([ainput0, ainput1])
+    bobout = bob([aliceout, binput1])  # bob sees ciphertext AND key
+    eveout = eve(aliceout)  # eve doesn't see the key [aliceout, ainput1]
 
-eoutput = Flatten()(econv4)  # Eve's attempt at guessing the plaintext
+    eveloss = K.mean(K.sum(K.abs(ainput0 - eveout), axis=-1))
+    bobloss = K.mean(K.sum(K.abs(ainput0 - bobout), axis=-1))
 
-eve = Model(einput, eoutput, name='eve')
+    abeloss = bobloss + K.square(m_bits/2 - eveloss) / \
+        ((m_bits//2)**2)  # alice-bob loss
 
-# Loss and optimizer
-aliceout = alice([ainput0, ainput1])
-bobout = bob([aliceout, binput1])  # bob sees ciphertext AND key
-eveout = eve(aliceout)  # eve doesn't see the key [aliceout, ainput1]
+    # Build and compile the ABE model, used for training Alice-Bob networks
+    #
+    abemodel = Model([ainput0, ainput1, binput1],
+                     bobout, name='abemodel')
+    abemodel.add_loss(abeloss)
 
-eveloss = K.mean(K.sum(K.abs(ainput0 - eveout), axis=-1))
-bobloss = K.mean(K.sum(K.abs(ainput0 - bobout), axis=-1))
+    # alice.compile(loss='mse', optimizer='sgd')
+    # beoptim = RMSprop(lr=0.001)
+    # eveoptim = RMSprop(lr=0.001)
+    abemodel.compile(optimizer='RMSprop')
 
-abeloss = bobloss + K.square(m_bits/2 - eveloss) / \
-    ((m_bits//2)**2)  # alice-bob loss
-
-# Build and compile the ABE model, used for training Alice-Bob networks
-#
-abemodel = Model([ainput0, ainput1, binput1],
-                 bobout, name='abemodel')
-abemodel.add_loss(abeloss)
-
-
-# alice.compile(loss='mse', optimizer='sgd')
-# beoptim = RMSprop(lr=0.001)
-# eveoptim = RMSprop(lr=0.001)
-abemodel.compile(optimizer='RMSprop')
-
-# Build and compile the Eve model, used for training Eve net (with Alice frozen)
-#
-alice.trainable = False
-evemodel = Model([ainput0, ainput1], eveout, name='evemodel')
-evemodel.add_loss(eveloss)
-evemodel.compile(optimizer='RMSprop')
+    # Build and compile the Eve model, used for training Eve net (with Alice frozen)
+    #
+    alice.trainable = False
+    evemodel = Model([ainput0, ainput1], eveout, name='evemodel')
+    evemodel.add_loss(eveloss)
+    evemodel.compile(optimizer='RMSprop')
