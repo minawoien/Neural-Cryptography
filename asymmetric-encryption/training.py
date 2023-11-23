0508c43dcf2f611dace50fd4038441442107f799
minawoien <mc.woien@stud.uis.no>
2023-11-23 10:25:30 +0100

    test 1 and 2 key 384

--- asymmetric-encryption/training.py
+++ asymmetric-encryption/training.py
@@ -12,7 +12,7 @@ import numpy as np
 from networks import alice, bob, eve, abemodel, m_train, m_bits, evemodel
 from key.ElipticCurve import generate_key_pair, curve
 
-i = 1
+i = 3
 curve = curve.name
 
 evelosses = []
